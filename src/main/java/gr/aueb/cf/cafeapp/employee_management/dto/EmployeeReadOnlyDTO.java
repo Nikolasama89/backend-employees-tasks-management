5b708b8e9415baa28c5061ff39dc63c0df00939e
Nikos <niqos89@gmail.com>
2025-07-18 22:28:00 +0300

    feat: add EmployeeReadOnlyDTO

---
+++ src/main/java/gr/aueb/cf/cafeapp/employee_management/dto/EmployeeReadOnlyDTO.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.cafeapp.employee_management.dto;
+
+import gr.aueb.cf.cafeapp.employee_management.core.enums.JobTitle;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmployeeReadOnlyDTO {
+    private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String uuid;
+    private String firstname;
+    private String lastname;
+    private String vat;
+    private String region;
+    private Long regionId;
+    private String email;
+    private Long userId;
+    private JobTitle jobTitle;
+}
