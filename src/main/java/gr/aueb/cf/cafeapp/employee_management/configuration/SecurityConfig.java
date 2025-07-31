72b9a3baf2479a5be569800c91c3f10014b9a6b1
Nikos <niqos89@gmail.com>
2025-07-31 23:02:24 +0300

    feat: add UserDetailsService bean to load users from database

--- src/main/java/gr/aueb/cf/cafeapp/employee_management/configuration/SecurityConfig.java
+++ src/main/java/gr/aueb/cf/cafeapp/employee_management/configuration/SecurityConfig.java
@@ -7,8 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,6 +22,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // ΓΙΑ ΝΑ ΠΑΙΡΝΕΙ ΤΟΝ ΑΡΧΙΚΟ ΜΑ
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository repo) {
+        return username -> repo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
     }
 
     // DISABLING AUTH FOR TESTING ENDPOINTS
@@ -39,16 +49,18 @@ public class SecurityConfig {
                         // Employees:
                         // Δημιουργία/Ενημέρωση/Διαγραφή μόνο ADMIN
                         // Ανάγνωση (GET) επιτρέπεται και σε EMPLOYEE
-                        .requestMatchers(HttpMethod.POST, "/api/users/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole("ADMIN", "EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasAnyRole("ADMIN", "EMPLOYEE")
 
                         .anyRequest().authenticated()
-                );
+                )
+                .httpBasic(Customizer.withDefaults());  // ενεργοποιηση Basic Auth
         return http.build();
     }
 
+    // ΓΙΑ ΝΑ ΕΧΟΥΜΕ ΕΝΑΝ ΑΡΧΙΚΟ ADMIN ΟΤΑΝ ΦΟΡΤΩΝΕΙ Η ΕΦΑΡΜΟΓΗ
     @Bean
     CommandLineRunner seedAdmin(UserRepository repo, PasswordEncoder encoder) {
         return args -> {
