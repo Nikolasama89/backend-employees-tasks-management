e90c914c5106ac4204778b4d208e8a45ebaa328d
Nikos <niqos89@gmail.com>
2025-08-03 21:38:37 +0300

    feat: add logging for AuthenticationService with successful authentications at INFO level and failed attempts at WARN level

---
+++ src/main/java/gr/aueb/cf/cafeapp/employee_management/authentication/AuthenticationService.java
@@ -0,0 +1,51 @@
+package gr.aueb.cf.cafeapp.employee_management.authentication;
+
+import gr.aueb.cf.cafeapp.employee_management.core.exceptions.EntityInvalidArgumentException;
+import gr.aueb.cf.cafeapp.employee_management.dto.AuthenticationRequestDTO;
+import gr.aueb.cf.cafeapp.employee_management.dto.AuthenticationResponseDTO;
+import gr.aueb.cf.cafeapp.employee_management.model.User;
+import gr.aueb.cf.cafeapp.employee_management.repository.UserRepository;
+import gr.aueb.cf.cafeapp.employee_management.security.JwtService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public AuthenticationResponseDTO authenticate(AuthenticationRequestDTO dto) {
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword())
+            );
+
+            log.info("User {} successfully authenticated", dto.getUsername());
+
+            User user = userRepository.findByUsername(auth.getName())
+                    .orElseThrow(() -> new EntityNotFoundException("User not Found"));
+
+            String token = jwtService.generateToken(user.getUsername(), user.getRole().name());
+
+            return new AuthenticationResponseDTO(
+                    user.getUsername(),
+                    token
+            );
+        } catch (AuthenticationException e) {
+            log.warn("Authentication failed for user {}: {}", dto.getUsername(), e.getMessage());
+            throw e;
+        }
+
+    }
+}
