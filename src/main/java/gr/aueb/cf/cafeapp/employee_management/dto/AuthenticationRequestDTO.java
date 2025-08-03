d16e3d2196218878ee40c50c28b6b30f8d9fabb2
Nikos <niqos89@gmail.com>
2025-08-03 21:27:39 +0300

    fix: change to NotBlank for not having empty password or username

--- src/main/java/gr/aueb/cf/cafeapp/employee_management/dto/AuthenticationRequestDTO.java
+++ src/main/java/gr/aueb/cf/cafeapp/employee_management/dto/AuthenticationRequestDTO.java
@@ -1,5 +1,6 @@
 package gr.aueb.cf.cafeapp.employee_management.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthenticationRequestDTO {
 
-    @NotNull
+    @NotBlank(message = "Username cannot be blank")
     private String username;
 
-    @NotNull
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 }
