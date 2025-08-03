843c41f8d8e56b185d52b56baeccf08c3f2e40c2
Nikos <niqos89@gmail.com>
2025-08-03 23:38:11 +0300

    feat: add SecurityFilterChain setup with JWT

--- src/main/java/gr/aueb/cf/cafeapp/employee_management/configuration/SecurityConfig.java
+++ src/main/java/gr/aueb/cf/cafeapp/employee_management/configuration/SecurityConfig.java
@@ -1,8 +1,11 @@
 package gr.aueb.cf.cafeapp.employee_management.configuration;
 
+import gr.aueb.cf.cafeapp.employee_management.authentication.AuthenticationService;
+import gr.aueb.cf.cafeapp.employee_management.authentication.JwtAuthenticationFilter;
 import gr.aueb.cf.cafeapp.employee_management.core.enums.Role;
 import gr.aueb.cf.cafeapp.employee_management.model.User;
 import gr.aueb.cf.cafeapp.employee_management.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +15,20 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationService authService;
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -38,8 +47,13 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                                // Swagger/OpenAPI UI & docs
+                        // public auth endpoint
+                        .requestMatchers("/api/auth/**").permitAll()
+
+                        // Swagger/OpenAPI UI & docs
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
 
                         // Users: ΜΟΝΟ ADMIN
@@ -55,10 +69,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/employees/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/employees/**").hasAnyRole("ADMIN", "EMPLOYEE")
-
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());  // ενεργοποιηση Basic Auth
+                .httpBasic(Customizer.withDefaults())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
