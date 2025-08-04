00162417b19944d1304353914060dd014db4e35a
Nikos <niqos89@gmail.com>
2025-08-04 23:42:01 +0300

    refactor: add OpenAPI configuration with security scheme

--- src/main/java/gr/aueb/cf/cafeapp/employee_management/configuration/OpenApiConfig.java
+++ src/main/java/gr/aueb/cf/cafeapp/employee_management/configuration/OpenApiConfig.java
@@ -3,6 +3,11 @@ package gr.aueb.cf.cafeapp.employee_management.configuration;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -17,5 +22,20 @@ import org.springframework.context.annotation.Configuration;
                 )
         )
 )
+
+
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
 }
